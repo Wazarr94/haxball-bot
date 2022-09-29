63b6e612e79d25352aed12f953b2c696b968cc1e
Wazarr <jeje_04@live.com>
2022-09-29 18:35:46 +0200

    parameters to keep downloads and local storage in the project

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({args: ["--disable-features=WebRtcHideLocalIpsWithMdns"]});
+  const browser = await puppeteer.launch({
+    userDataDir: "./user-data-dir",
+    args: ["--disable-features=WebRtcHideLocalIpsWithMdns"],
+  });
   const page = await browser.newPage();
-  await page.goto('https://haxball.com/headless', { waitUntil: 'networkidle2' });
 
-  page.on('console', async (msg) => {
+  const client = await page.target().createCDPSession();
+
+  await client.send("Page.setDownloadBehavior", {
+    behavior: "allow",
+    downloadPath: "./downloads",
+  });
+
+  await page.goto("https://haxball.com/headless", {
+    waitUntil: "networkidle2",
+  });
+
+  page.on("console", async (msg) => {
     const msgArgs = msg.args();
     for (let i = 0; i < msgArgs.length; ++i) {
       console.log(await msgArgs[i].jsonValue());
     }
   });
 
-  page.on('error', async (msg) => {
+  page.on("error", async (msg) => {
     const msgArgs = msg.args();
     for (let i = 0; i < msgArgs.length; ++i) {
       console.log(await msgArgs[i].jsonValue());
     }
   });
 
-  page.addScriptTag({ path: "./haxball_script.js" })
-
+  page.addScriptTag({ path: "./haxball_script.js" });
 })();
